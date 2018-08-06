fdf9cd1b01b11c359db2aef4a0cc32ad526f718a
Justin Sobieski <jmksobieski@gmail.com>
2018-08-06 14:41:40 -0400

    Update guildowner.py

--- cogs/guildowner.py
+++ cogs/guildowner.py
@@ -59,6 +59,23 @@ class GuildOwnerCog:
         else:
             await ctx.send(f'I couldnt find a cog named {cog}')
 
+    @commands.command(aliases=['sp'])
+    @is_owner()
+    async def set_prefix(self, ctx, prefix: str=None):
+        if not prefix:
+            prefix = self.bot.config.data.get('servers').get(str(ctx.guild.id)).get('prefix')
+            await ctx.send(f'current prefix is {prefix}')
+            return
+        else:
+            if len(prefix) >= 3:
+                await ctx.send('Prefix length too long.')
+                return
+
+            self.bot.config.data['servers'][str(ctx.guild.id)]['prefix'] = prefix
+            self.bot.config.save()
+            await ctx.send(f'Prefix updated to {prefix}')
+
+
 
 def setup(bot):
     bot.add_cog(GuildOwnerCog(bot))
