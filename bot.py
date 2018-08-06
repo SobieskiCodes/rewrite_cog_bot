c48e0a0f342f897b01d14c8bf56f6f27fb420a08
Justin Sobieski <jmksobieski@gmail.com>
2018-08-06 14:27:20 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
 import discord
 import os
+import traceback
 from discord.ext import commands
 from pathlib import Path
 if not os.path.isdir('cogs/data'):
     os.makedirs('cogs/data')
 from cogs.util import pyson
 
-config = pyson.Pyson('data/config/startup.json')
-token = config.data.get('config').get('token')
 
 
 def get_prefix(bot, message):
@@ -26,6 +25,27 @@ async def on_ready():
     print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}')
     game = discord.Game("with building the cogs")
     await bot.change_presence(status=discord.Status.idle, activity=game)
+
+
+@bot.event
+async def on_command_error(error):
+    if not isinstance(error, commands.errors.CheckFailure):
+        try:
+            raise error
+        except Exception as error:
+            tb = traceback.format_exc()
+            print(error, tb)
+    else:
+        pass
+
+
+@bot.check
+async def __before_invoke(ctx):
+    cog_name = ctx.command.cog_name
+    if not cog_name:
+        cog_name = "None"
+    if cog_name in bot.config.data.get('servers').get(str(ctx.guild.id)):
+        return True
 
 
 def load_extensions():
@@ -47,5 +67,7 @@ def load_extensions():
                 print('Failed to load extension {}\n{}'.format(extension, exc))
 
 
+bot.config = pyson.Pyson('data/config/startup.json')
+token = bot.config.data.get('config').get('token')
 load_extensions()
 bot.run(token, bot=True, reconnect=True)
